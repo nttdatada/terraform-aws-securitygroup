0b14ebd6305b88e497f3cefd2c69108ca1284fad
Marcos Cianci <marcos.lopescianci@nttdata.com>
2022-03-15 00:37:24 -0300

    Update output values

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,6 @@
 output "security_group_id" {    
     
     description = "AWS Security Group ID"
-    value       = aws_security_group.securitygroups.*.id 
+    value       = values(aws_security_group.securitygroups).*.id 
 }
 
